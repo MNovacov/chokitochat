0a364c91c9d2ffae501c8dea12b06f17c961c161
MNovacov <mnovacov@hotmail.com>
2025-05-08 12:01:43 -0400

    Estilo mejorado, correcciones de transparencia y colores

--- src/components/ColorSelector.jsx
+++ src/components/ColorSelector.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 
 export default function ColorSelector({ onChange, currentPalette }) {
+  const hexToRgba = (hex, alpha = 0.31) => {
+    hex = hex.replace('#', '');
+  
+    if (hex.length === 3) {
+      hex = hex.split('').map(char => char + char).join('');
+    }
+  
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+  
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  };
+
   const handleColorChange = (colorType, colorValue) => {
-    onChange(colorType, colorValue);
+    const rgbaColor = hexToRgba(colorValue, 0.35); 
+    onChange(colorType, rgbaColor);
   };
 
   const colors = [
-    { name: 'primary', color: '#222' },
-    { name: 'secondary', color: '#78b784' },
-    { name: 'tertiary', color: '#94a0c7' },
-    { name: 'quaternary', color: '#4f9cdb' },
+    { name: 'primary', color: '#000000' },
+    { name: 'secondary', color: '#00F2FB' },
+    { name: 'tertiary', color: '#00b71d' },
+    { name: 'quaternary', color: '#1e1e62' },
   ];
 
   return (

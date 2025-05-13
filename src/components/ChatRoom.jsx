b63d4206b89e5d0de30eb88c7faf8d5b8d3d905e
MNovacov <mnovacov@hotmail.com>
2025-05-13 17:12:29 -0400

    Fix fluidez

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -121,7 +121,7 @@ export default function ChatRoom({ palette }) {
   const sendImageMessage = async (file) => {
     const formData = new FormData();
     formData.append("image", file);
-    formData.append("expiration", "180");
+    formData.append("expiration", "30");
     formData.append("key", imgbbAPIKey);
     const response = await fetch("https://api.imgbb.com/1/upload", { method: "POST", body: formData });
     const result = await response.json();
